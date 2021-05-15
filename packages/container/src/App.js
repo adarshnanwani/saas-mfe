21c75ce5fd16f49ff4615cba3c1990709d7338c5
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-16 00:09:58 +0530

    Add Header to CONTAINER.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <>
-      <h1>Hi there!</h1>
-      <hr />
-      <hr />
+    <Router>
+      <Header />
       <MarketingApp />
-    </>
+    </Router>
   );
 };
