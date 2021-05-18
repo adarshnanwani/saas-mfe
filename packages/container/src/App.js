588bbb07cf098c1991620f72ab4baef96e302e4e
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-18 23:21:06 +0530

    Add client side auth.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
@@ -13,13 +13,20 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
       <Router>
-        <Header />
+        <Header
+          isSignedIn={isSignedIn}
+          onSignOut={() => setIsSignedIn(false)}
+        />
         <Suspense fallback={<Progress />}>
           <Switch>
-            <Route path='/auth' component={AuthLazy} />
+            <Route path='/auth'>
+              <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+            </Route>
             <Route path='/' component={MarketingLazy} />
           </Switch>
         </Suspense>
