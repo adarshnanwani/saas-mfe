89a05b735192ac3604ac4fad09b520a3ed6517fe
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-18 23:59:06 +0530

    Setup DASHBOARD app.

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,33 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createMemoryHistory, createBrowserHistory } from 'history';
-import App from './App';
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
 
-const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
-
-  if (onNavigate) history.listen(onNavigate);
-
-  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
-
-  return {
-    onParentNavigate({ pathname: nextPathname }) {
-      const { pathname } = history.location;
-      if (pathname !== nextPathname) history.push(nextPathname);
-    },
-  };
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
 };
 
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_auth-dev-root');
+  const devRoot = document.querySelector('#_dashboard-dev-root');
+
   if (devRoot) {
-    mount(devRoot, {
-      defaultHistory: createBrowserHistory(),
-    });
+    mount(devRoot);
   }
 }
 
